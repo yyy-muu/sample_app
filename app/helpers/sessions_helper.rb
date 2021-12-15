3aab219cc0e0a71eba1a9462946f691e3044b14f
yyy-muu <rirakkuma.0918.y@gmail.com>
2021-12-15 15:05:22 +0000

    追加：ユーザログイン機能を実装

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+  
+  # ユーザ情報を引数として渡し、ログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  
+end
