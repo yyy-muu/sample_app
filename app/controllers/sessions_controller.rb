f59369e28f90f586045170d50289a6bff25e305c
yyy-muu <rirakkuma.0918.y@gmail.com>
2021-12-22 15:21:17 +0000

    追加：カレントユーザ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,19 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
     
     # if user && user.authenticate(params[:session][:password])
-    if user&.authenticate(params[:session][:password])
-      log_in user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
       # user_url(user)ルーティングに変換している
       
       # チェックボックスがオンのときにユーザーを記憶し、
       # オフのときには記憶しない
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       
       # ログイン後、ユーザ情報ページにリダイレクトする
-      redirect_to user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
