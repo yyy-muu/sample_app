249f8a06011b3131fbe9f4a4fe0f133390d45b26
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-01-23 10:30:53 +0000

    追加：フィード機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -122,11 +122,12 @@ class User < ApplicationRecord
   # フォローしているユーザーに対応するidの配列を持つマイクロポストをすべて選択（select）する
   # ユーザーのステータスフィードを返す
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE follower_id = :user_id"
-    # whereメソッド内の変数に、キーと値のペアを使う
-    Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    
+    # SQLのLEFT JOIN
+    Micropost.left_outer_joins(user: :followers)
+             .where(part_of_feed, { id: id }).distinct
+             .includes(:user, image_attachment: :blob)
   end
 
   # ユーザーをフォローする
